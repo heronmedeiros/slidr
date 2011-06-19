96da8bde9e7619a0bc2079b2d62407c6b73b5388
heron lee medeiros <heron.medeiros@gmail.com>
2011-06-19 00:48:38 -0300

    include run_tests.js and test/handler_test with a simple test

---
+++ tests/handler_test.js
@@ -0,0 +1,12 @@
+var testCase = require("nodeunit").testCase
+  , handlers = require("../handlers.js")   
+;
+
+
+module.exports = testCase({
+  setUp: function(calback){ calback(); },
+  tearDown: function(calback){ calback(); }
+
+
+
+})
