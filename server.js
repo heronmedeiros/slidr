ffab6f2ea39da3c459c7d6f5b22ce5286bc7a1ce
heronmedeiros <heron.medeiros@gmail.com>
2011-06-25 01:23:50 -0300

    Refactoring slides' creation

--- server.js
+++ server.js
@@ -12,21 +12,21 @@ var express = require("express")
   , users = {}
 ;
 
-app.configure(function(){
+app.configure(function () {
   app.use(express.methodOverride());
   app.use(express.bodyParser());
   app.use(express.static(__dirname + "/public"));
 });
 
-app.get("/", function(req, res){
-  Presentation.find({}, function(err, docs){
+app.get("/", function(req, res ) {
+  Presentation.find({}, function (err, docs ) {
     res.render("index.ejs", {layout: "layout/site.ejs", presentations: docs});
   });
 });
 
-app.get("/create", function(req, res){
+app.get("/create", function (req, res ) {
   var presentation = new Presentation();
-  presentation.save(function(err){
+  presentation.save(function (err ) {
     if (err) {
       throw err;
     }
@@ -35,8 +35,8 @@ app.get("/create", function(req, res){
   });
 });
 
-app.get("/speaker/:id", function(req, res, next){
-  Presentation.findById(req.params.id, function(err, presentation){
+app.get("/speaker/:id", function (req, res, next ) {
+  Presentation.findById(req.params.id, function (err, presentation ) {
     if (err || !presentation) { return next(err); }
 
     res.render("create.ejs", {
@@ -47,46 +47,40 @@ app.get("/speaker/:id", function(req, res, next){
   });
 });
 
-app.post("/speaker/:id/upload", function(req, res, next){
-  req.form.on("progress", function(received, expected){
+app.post("/speaker/:id/upload", function (req, res, next ) {
+  req.form.on("progress", function (received, expected ) {
     console.log("received: " + received);
     console.log("expected: " + expected);
     console.log((received / expected * 100) + "%");
   });
 
-  req.form.complete(function(err, fields, files){
+  req.form.complete(function (err, fields, files ) {
     if (err) {
       return next(error);
     }
 
-    var destiny = __dirname + "/public/slides/" + req.params.id
-      , cmd = [
-          "rm -rf " + destiny
-        , "mkdir -p " + destiny
-        , "gs -q -dNOPAUSE -dBATCH -sDEVICE=pngalpha -sOutputFile=" + destiny + "/slides%d.png " + files.slides.path
-        , "ls " + destiny + " | wc -l"
-      ].join(" && ")
-    ;
+    var cmd = makeSlides(req, files);
 
     console.log("== command: ", cmd);
 
-    exec(cmd, function(err, stdout, stderr){
-      Presentation.findById(req.params.id, function(err, presentation){
-        if (err || !presentation) { return next(err); }
+    exec(cmd, function (err, stdout, stderr ) {
+      Presentation.findById(req.params.id, function (err, presentation ) {
+        ppt(err,res, presentation, stdout);
+        //if (err || !presentation) { return next(err); }
 
-        presentation.slides = parseInt(stdout.replace(/[^\d]/g, ""), 10);
+        //presentation.slides = parseInt(stdout.replace(/[^\d]/g, ""), 10);
         
-        presentation.save(function(err){
-          if (err) { throw err };
-          res.redirect("/speaker/" + presentation.id);
-        });
+        //presentation.save(function (err ) {
+          //if (err) { throw err };
+          //res.redirect("/speaker/" + presentation.id);
+        //});
       });
     });
   });
 });
 
-app.get("/attend/:presentation_id", function(req, res, next){
-  Presentation.findById(req.params.presentation_id, function(err, presentation){
+app.get("/attend/:presentation_id", function (req, res, next ) {
+  Presentation.findById(req.params.presentation_id, function (err, presentation ) {
     if (err || !presentation) { return next(err); }
 
     res.render("attend.ejs", {
@@ -96,8 +90,8 @@ app.get("/attend/:presentation_id", function(req, res, next){
   });
 });
 
-app.post("/attend/:presentation_id", function(req, res, next){
-  Presentation.findById(req.params.presentation_id, function(err, presentation){
+app.post("/attend/:presentation_id", function (req, res, next ) {
+  Presentation.findById(req.params.presentation_id, function (err, presentation ) {
     if (err || !presentation) { return next(err); }
 
     var attendee = new Attendee({
@@ -107,18 +101,18 @@ app.post("/attend/:presentation_id", function(req, res, next){
 
     presentation.attendees.push(attendee);
 
-    presentation.save(function(err){
+    presentation.save(function (err ) {
       if (err) { throw err; }
       res.redirect("/attend/" + presentation.id + "/" + attendee._id);
     });
   });
 });
 
-app.get("/attend/:presentation_id/:id", function(req, res, next){
-  Presentation.findById(req.params.presentation_id, function(err, presentation){
+app.get("/attend/:presentation_id/:id", function (req, res, next ) {
+  Presentation.findById(req.params.presentation_id, function (err, presentation ) {
     if (err || !presentation) { return next(err); }
 
-    var attendee = presentation.attendees.filter(function(item){
+    var attendee = presentation.attendees.filter(function (item ) {
       return item.id == req.params.id
     })[0];
 
@@ -136,18 +130,18 @@ app.get("/attend/:presentation_id/:id", function(req, res, next){
 });
 
 appWS = ws.createServer(function (socket) {
-  var write = function(websocket, message, payload) {
+  var write = function (websocket, message, payload) {
     payload.type = message;
     websocket.write(JSON.stringify(payload));
   }
 
-  var emitterHandler = function(message, payload) {
+  var emitterHandler = function (message, payload) {
     console.log(arguments);
     console.log("== emitting message: ", message);
     write(socket, message, payload);
   }
 
-  var emit = function(message, payload) {
+  var emit = function (message, payload) {
     emitter.emit(message, message, payload);
   }
 
@@ -186,3 +180,35 @@ appWS = ws.createServer(function (socket) {
 
 app.listen(2345);
 appWS.listen(2346);
+
+
+private:
+ppt = function(err, res, presentation, _stdout)
+{
+  if (err || !presentation) { return next(err); }
+
+  presentation.slides = parseInt(_stdout.replace(/[^\d]/g, ""), 10);
+  presentation.save(function (err ) {
+      if (err) { throw err };
+      res.redirect("/speaker/" + presentation.id);
+  });
+};
+
+
+destiny = function (req ) {
+  return __dirname + "/public/slides/" + req.params.id
+}
+
+makeSlides =  function (req, files)
+{
+  dest = destiny(req);
+    cmd = [
+      "rm -rf " + dest
+      , "mkdir -p " + dest
+      , "gs -q -dNOPAUSE -dBATCH -sDEVICE=pngalpha -sOutputFile=" + dest + "/slides%d.png " + files.slides.path
+      , "ls " + dest + " | wc -l"
+      ].join(" && ")
+    ;
+
+  return cmd
+}
